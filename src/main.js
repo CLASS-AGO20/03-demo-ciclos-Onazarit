a00557acbe38beed19a54552b39684a0e0f2b441
Onazarit <onazarit@ucol.mx>
2020-12-06 15:07:06 -0600

    Implemented function sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+
+    sumarParesFor(){
+        let total=0;
+        for(let i=2; i<=20; i+=2){
+            total = total + i;
+        }
+    return(total);
+    }
+
+    
+}
 
 let app = new App();
+console.log("La suma de pares del 2 - 20 es: "+app.sumarParesFor());
