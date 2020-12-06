1ead07b75fbd925e6a50ce6c7f6137e0ee9ee492
Onazarit <onazarit@ucol.mx>
2020-12-06 15:08:58 -0600

    Implemented function contarImparesFor

--- src/main.js
+++ src/main.js
@@ -8,8 +8,18 @@ export default class App {
     return(total);
     }
 
+    contarImparesFor(inicio,final){
+        let totalImpares = 0;
+        for(let i=inicio;i<=final;i++){
+            if(i % 2 != 0)
+                totalImpares++;
+        }
+        return(totalImpares);
+    }
+
     
 }
 
 let app = new App();
 console.log("La suma de pares del 2 - 20 es: "+app.sumarParesFor());
+console.log("Entre el 0 y el 6 hay "+app.contarImparesFor(2,10)+" numeros impares");
